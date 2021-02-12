df56285dfab4d8cf154e8b6c0ffed13b8af4d4c0
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-02-12 14:41:30 +1300

    audio fiule added

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { navigate } from '../actions'
+import Sound from 'react-sound'
 
 
 
 const Home = (props) => {
+
+    const [playing, setPlaying] = useState(false);
+
+    // useEffect (() => {
+    //     setPlaying(true)
+    // }, [])
 
     const handleClick = (target) => {
         props.dispatch(navigate(target))
     }
 
     return (
-        <div>
+        <div onMouseMove={() => setPlaying(true)}>
             <h1>Welcome to the Office</h1>
 
             <img src="https://media.giphy.com/media/yCY0QS4BhonQ6KPPGn/giphy.gif" alt="a gif of an office related thing happening"/>
@@ -19,6 +26,13 @@ const Home = (props) => {
             <button onClick={() => handleClick('form')}>jump to form</button>
             <button onClick={() => handleClick('all-matches')}>jump to all matches</button>
             </div>
+
+            <Sound
+            url='/Kalimba.mp3'
+            autoLoad={true}
+            playStatus={playing ? Sound.status.PLAYING : Sound.status.STOPPED}
+            playFromPosition={1000} 
+            />
 
         </div>
     )
